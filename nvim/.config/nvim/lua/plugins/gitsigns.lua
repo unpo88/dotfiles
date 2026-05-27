bbc00e17a110f7f76c414647dc7b284bc52e2e16
whatsup@lemonbase.com <whatsup@lemonbase.com>
2026-05-27 17:35:20 +0900

    feat(nvim): add gitsigns current_line_blame config

---
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,11 @@
+---@type LazySpec
+return {
+  "lewis6991/gitsigns.nvim",
+  opts = {
+    current_line_blame = true,
+    current_line_blame_opts = {
+      delay = 300,
+      virt_text_pos = "eol",
+    },
+  },
+}
